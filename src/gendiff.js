22184fea89db6b59dd7635df2e01bb4d378a0ed6
root <root@DESKTOP-6GBLENF>
2023-05-10 14:53:35 +0300

    try6

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,28 +3,29 @@ import { cwd } from 'node:process';
 import path from 'node:path';
 import _ from 'lodash';
 
+const addDiff = (key, acc, object1, object2) => {
+  if (_.has(object1, key) && _.has(object2, key) && object1[key] === object2[key]) {
+    return [...acc, `    ${key}: ${object1[key]}`];
+  }
+  if (_.has(object1, key) && _.has(object2, key) && object1[key] !== object2[key]) {
+    return [...acc, `  - ${key}: ${object1[key]}`, `  + ${key}: ${object2[key]}`];
+  }
+  if (_.has(object1, key) && !_.has(object2, key)) {
+    return [...acc, `  - ${key}: ${object1[key]}`];
+  }
+  if (!_.has(object1, key) && _.has(object2, key)) {
+    return [...acc, `  + ${key}: ${object2[key]}`];
+  }
+  return null;
+};
+
 const genDiff = (filePath1, filePath2) => {
   const current = cwd();
-  const file1 = readFileSync(path.resolve(current, filePath1));
-  const file2 = readFileSync(path.resolve(current, filePath2));
-  const obj1 = JSON.parse(file1);
-  const obj2 = JSON.parse(file2);
+  const obj1 = JSON.parse(readFileSync(path.resolve(current, filePath1), 'utf-8'));
+  const obj2 = JSON.parse(readFileSync(path.resolve(current, filePath2), 'utf-8'));
   const keys = _.uniq([...Object.keys(obj2), ...Object.keys(obj1)]);
   const sortedKeys = _.sortBy(keys);
-  const arrOfDiffs = sortedKeys.reduce((acc, key) => {
-    if (_.has(obj1, key) && _.has(obj2, key) && obj1[key] === obj2[key]) {
-      return [...acc, `    ${key}: ${obj1[key]}`];
-    }
-    if (_.has(obj1, key) && _.has(obj2, key) && obj1[key] !== obj2[key]) {
-      return [...acc, `  - ${key}: ${obj1[key]}`, `  + ${key}: ${obj2[key]}`];
-    }
-    if (_.has(obj1, key) && !_.has(obj2, key)) {
-      return [...acc, `  - ${key}: ${obj1[key]}`];
-    }
-    if (!_.has(obj1, key) && _.has(obj2, key)) {
-      return [...acc, `  + ${key}: ${obj2[key]}`];
-    }
-  }, []);
+  const arrOfDiffs = sortedKeys.reduce((acc, key) => addDiff(key, acc, obj1, obj2), []);
   const strOfDiffs = arrOfDiffs.join('\n');
   return `{\n${strOfDiffs}\n}`;
 };
